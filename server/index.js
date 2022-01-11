519fb1d6cb8c7eac782b67eda11f02de4971233e
Francesco Scheffczyk <francesco.scheffczyk@gmail.com>
2022-01-11 22:14:40 +0100

    Add tweetIDs from every month to pipeline

--- server/index.js
+++ server/index.js
@@ -1,16 +1,47 @@
 import app from 'express';
 import dotenv from 'dotenv'
 import got from 'got';
+import fs from'fs';
+import readline from 'readline';
+import LineReader from 'line-reader';
 import { TwitterApi } from 'twitter-api-v2';
 import { sentimentAnalysis } from './sentimentAnalysis.js'
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
+console.log(`Server running at ${port}`);
+
+let urls = [
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-01/coronavirus-tweet-id-2020-01-21-22.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-02/coronavirus-tweet-id-2020-02-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-03/coronavirus-tweet-id-2020-03-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-04/coronavirus-tweet-id-2020-04-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-05/coronavirus-tweet-id-2020-05-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-06/coronavirus-tweet-id-2020-06-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-07/coronavirus-tweet-id-2020-07-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-08/coronavirus-tweet-id-2020-08-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-09/coronavirus-tweet-id-2020-09-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-10/coronavirus-tweet-id-2020-10-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-11/coronavirus-tweet-id-2020-11-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2020-12/coronavirus-tweet-id-2020-12-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-01/coronavirus-tweet-id-2021-01-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-02/coronavirus-tweet-id-2021-02-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-03/coronavirus-tweet-id-2021-03-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-04/coronavirus-tweet-id-2021-04-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-05/coronavirus-tweet-id-2021-05-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-06/coronavirus-tweet-id-2021-06-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-07/coronavirus-tweet-id-2021-07-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-08/coronavirus-tweet-id-2021-08-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-09/coronavirus-tweet-id-2021-09-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-10/coronavirus-tweet-id-2021-10-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-11/coronavirus-tweet-id-2021-11-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-12/coronavirus-tweet-id-2021-12-01-01.txt',
+    'https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2022-01/coronavirus-tweet-id-2022-01-01-01.txt'
+]
+
 let tweetIDs = []
 let tweets = []
-
-console.log(`Server running at ${port}`);
 
 const twitterApi = new TwitterApi({
     appKey: process.env.APP_KEY,
@@ -19,16 +50,27 @@ const twitterApi = new TwitterApi({
     accessSecret: process.env.ACCESS_TOKEN_SECRET,
 });
 
-async function collectTweetIDs()  {
-    let rawTextData = await got(`https://raw.githubusercontent.com/echen102/COVID-19-TweetIDs/master/2021-01/coronavirus-tweet-id-2021-01-01-01.txt`);
-    let rawJsonData = JSON.stringify(rawTextData.body);
-    let splittedJSON = rawJsonData.split('n');
+async function collectTweetIDs(files)  {
+    for (let i = 0; i < files.length; i++) {
+        const fileStream = got.stream(files[i]);
+  
+        const rl = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+        });
 
-    // Clean up and save to array
-    splittedJSON.forEach((element) => {
-        tweetIDs.push(element.slice(0, element.length-1));
-    })
-    tweetIDs[0] = tweetIDs[0].slice(1, tweetIDs[0].length)
+        let count = 0;
+    
+        for await (const line of rl) {
+            tweetIDs.push(line);
+            console.log(count);
+            count++;
+
+            if(count >= 10000) {
+                break
+            }
+        }
+    }
 }
 
 async function getTweets(tweetIDs) {
@@ -68,9 +110,10 @@ async function getSingleTweet(tweetID) {
     }
 };
 
-function init() {
-    collectTweetIDs()
+function init() {    
+    collectTweetIDs(urls)
     .then(async () => {
+        console.log(tweetIDs.length);
         await getTweets(tweetIDs);
     }).then(async () => {
         tweets.forEach(async (tweet) => {
